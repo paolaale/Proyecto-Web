e5efa45fa371070f2bb56c0da373bff30d43c9d6
Paola Villarreal García <31547279+paolaale@users.noreply.github.com>
2020-12-01 17:20:40 -0600

    lot of fixes :)

--- src/imagesProfileUrls.js
+++ src/imagesProfileUrls.js
@@ -3,28 +3,57 @@
 // STICH 
 const DEFAUTL_PROFILE = 'https://vignette.wikia.nocookie.net/lilostitchexperimentos/images/f/f6/Stitch_%28Lilo_%26_Stitch%29.svg.png/revision/latest/scale-to-width-down/340?cb=20150521210241&path-prefix=es';
 
-const capi = 'https://pre00.deviantart.net/e997/th/pre/i/2011/358/6/0/captain_america_round_shield_by_lord_giampietro-d4k4t46.png';
+const capi = 'https://i.pinimg.com/originals/89/39/93/893993cf74bf7bd4c2f6cf445db31e7e.png';
 const spiderman = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fmarvelmovies%2Fimages%2F6%2F6a%2FSpidermanPromo2-TASM2.png%2Frevision%2Flatest%2Fscale-to-width-down%2F2000%3Fcb%3D20161114050322&f=1&nofb=1';
-const stich2 = 'https://www.vippng.com/png/full/18-185102_lilo-and-stitch-experiment-228-png-download-lilo.png';
-
+const tinkerbell = 'https://www.pngkey.com/png/full/439-4393966_imgenes-de-tinkerbell-con-fondo-transparente-descarga-tinkerbell.png';
+const ironman = 'https://assets.stickpng.com/images/58569c7a4f6ae202fedf2751.png';
+const hulk = 'https://vignette.wikia.nocookie.net/mugen-base-de-datos/images/f/f3/MUA3_Hulk.png/revision/latest?cb=20190830215815&path-prefix=es';
+const nemo = 'https://i.pinimg.com/originals/ca/6a/c5/ca6ac517b9df9ecbd69645ba273a9bd4.png';
+const simba = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1b0b6845-05a3-444f-8705-9ab7c7b67dac/dbx8gob-a3536514-4300-4243-9824-aed180e0976d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWIwYjY4NDUtMDVhMy00NDRmLTg3MDUtOWFiN2M3YjY3ZGFjXC9kYng4Z29iLWEzNTM2NTE0LTQzMDAtNDI0My05ODI0LWFlZDE4MGUwOTc2ZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.XDcoxHlKXAgue3ijuEnh84elEQisNKLgo9gxWMIbPXY';
+const yoda = 'https://assets.stickpng.com/images/5ee76df199588c0004aa6844.png';
+const mickey = 'https://pngimg.com/uploads/mickey_mouse/mickey_mouse_PNG97.png';
+const minnie = 'https://lh3.googleusercontent.com/proxy/eIPGRbQ9tGbPK3I0qcVRkZ0eY2yPSg-G-uGPXOBeZcQhsry-rRsUPXsVauRudsIUTtTrhZw3NDBikpGXGVdAX6lwySo4-qBZHNiPNo69jeF27MRcmvt7L2-nXarMw5D9qsEcfKXe';
+const buzz = 'https://i.pinimg.com/originals/8e/ca/cd/8ecacde97d69b2d18fdecb45ccb145ab.png';
+const darth = 'https://purepng.com/public/uploads/large/purepng.com-darth-vaderdarth-vaderanakin-skywalkerstar-wars-franchiseskywalker-17015277544928knzg.png';
+const durmiente = 'https://i.pinimg.com/originals/84/30/5c/84305c7b7ba7ae2575b8d9dac7283e3d.png';
+const bella = 'https://4.bp.blogspot.com/-YY4cl0YYxRQ/XJP7V6JhjUI/AAAAAAAAJUw/QH1xOxFAtxcMkrQeM7TipFCNSC8MiNK8ACEwYBhgL/s1600/Belle%2B5.png';
+const moana = 'https://assets.stickpng.com/images/588352782c9eb99faafea8a1.png';
+const elsa = 'https://lh3.googleusercontent.com/proxy/oCtXxbIx1hsXzamJouvqS4CHZzmA9Cp_gqz4HNSAgMyR664VFtcTaWCGzUY5USCnrKM2Vh2fLL-jBv3XIFWVIjNtSeyOM93XFZn9Gn2HKx9bcWcGAc8';
+const ana = 'https://i2.wp.com/todoprincesasdisney.com/wp-content/uploads/2014/01/Anna-Frozen-png.png?fit=299%2C459&ssl=1';
+const alegria = 'https://i.pinimg.com/originals/71/47/40/714740864f376a6978295f6d6b6d9199.png';
+const furia = 'https://vignette.wikia.nocookie.net/intensamente/images/8/8b/Angerirafuria.png/revision/latest/scale-to-width-down/340?cb=20150803140629&path-prefix=es';
+const donald = 'https://i.pinimg.com/originals/69/5f/5d/695f5d630d50cec01a21e968ec3f1ee9.png';
+const jessie = 'https://i.pinimg.com/originals/58/97/0b/58970b3c5e18894c5e24c98d18e7e20a.png';
+const mike = 'https://vignette.wikia.nocookie.net/pixar/images/6/6a/Mike_Wazowski.png/revision/latest?cb=20160131132812&path-prefix=es';
+const doc = 'https://assets.stickpng.com/images/5a5e14169fc8c9351ecc49d8.png';
+const jake = 'https://i.pinimg.com/originals/f1/e9/5e/f1e95ebd77858ed85d4022bd3d7fcde7.png';
 
 const IMGAGES_URL = [
     DEFAUTL_PROFILE,
     capi,
+    nemo,
+    alegria,
+    darth,
+    tinkerbell,
+    simba,
+    yoda,
     spiderman,
-    stich2,
-    DEFAUTL_PROFILE,
-    capi,
-    spiderman,
-    stich2,
-    DEFAUTL_PROFILE,
-    capi,
-    spiderman,
-    stich2,
-    DEFAUTL_PROFILE,
-    capi,
-    spiderman,
-    stich2,
+    buzz,
+    durmiente,
+    furia,
+    ironman,
+    mickey,
+    minnie,
+    donald,
+    bella,
+    moana,
+    elsa,
+    hulk,
+    ana,
+    jessie,
+    mike,
+    doc,
+    jake
 ];
 
 module.exports = {
